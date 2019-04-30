83d75776f4b9f9a442f7f84d97e0dda7c01e74b3
Ammar Ali <funnyguy118@gmail.com>
2019-04-30 14:56:38 -0400

    first deliverable

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,7 +3,31 @@ import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
+const baseURL = 'http://localhost:3000/stocks'
+
 class MainContainer extends Component {
+
+  state= {
+    allStocks : [],
+    myPortfolio: [],
+
+  }
+
+  componentDidMount() {
+    fetch(baseURL)
+    .then(r=>r.json())
+    .then(allStocks=>{
+      this.setState({
+        allStocks
+      })
+    })
+  }
+
+  togglePortfolio= stock => {
+    this.state.myPortfolio.includes(stock) ?
+    console.log('its in my porfolio already, lets try to remove it', stock)
+    : console.log('checking', stock.target)
+  }
 
   render() {
     return (
@@ -13,12 +37,15 @@ class MainContainer extends Component {
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer
+                allStocks={this.state.allStocks}
+                togglePortfolio={this.togglePortfolio}
+              />
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer myPortfolio={this.state.myPortfolio}/>
 
             </div>
           </div>
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -8,7 +8,13 @@ class StockContainer extends Component {
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          this.props.allStocks.map(stock => {
+            return <Stock
+                      stats={stock}
+                      key={stock.id}
+                      togglePortfolio={this.props.togglePortfolio}
+                    />
+          })
         }
       </div>
     );
